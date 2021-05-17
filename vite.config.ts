4e800da5efa0eac53681e002d71b18c4d7c9dab2
djytwy <676534074@qq.com>
2021-05-17 16:18:15 +0800

    修改vite的配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   // vite 配置新写法：真香
   resolve: {
+    // 两种引用写法
     alias : [
-      { find: '@', replacement: '/src' },
-      { find: 'views', replacement: '/src/views'},
+      { find: /@\//, replacement: path.join(__dirname, './src/') },
+      { find: 'views', replacement: path.join(__dirname, './src/views')},
       { find: 'components', replacement: '/src/components'}
     ]
   }
